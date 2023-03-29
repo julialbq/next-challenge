b3ed39e4f6621725dfeb3dd51e7381dee86b05e5
julia <julia.albq@gmail.com>
2023-03-29 17:47:34 -0300

    refactor: change Header to remove onClick functions and Link paths

--- src/ui/components/Header.jsx
+++ src/ui/components/Header.jsx
@@ -3,26 +3,25 @@ import { useViewState } from "../../application/hooks/useViewState";
 import Link from "next/link";
 
 export const Header = () => {
-  const { goToRooms, goToStudents, goToInventory } = useViewState();
-
+  
   return (
     <header className={cx.header}>
       <h1 className={cx.title}>Sistema Interno</h1>
 
       <nav className={cx.navbar}>
         <ul className={cx.navbarList}>
-          <Link href={`/`}>
-            <li onClick={() => goToRooms()} className={cx.navbarListItem}>
+          <Link href={`/rooms`}>
+            <li className={cx.navbarListItem}>
               Salas
             </li>
           </Link>
           <Link href={`/students/`}>
-            <li onClick={() => goToStudents()} className={cx.navbarListItem}>
+            <li className={cx.navbarListItem}>
               Alunos
             </li>
           </Link>
           <Link href="/inventory">
-            <li onClick={() => goToInventory()} className={cx.navbarListItem}>
+            <li className={cx.navbarListItem}>
               Inventário
             </li>
           </Link>
